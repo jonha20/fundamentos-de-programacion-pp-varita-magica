3c127c846d1ac816922905d32c0c7209b1f6ef3d
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-12 20:38:37 +0100

    Ejercicios Finalizados

--- scripts.js
+++ scripts.js
@@ -37,11 +37,47 @@ document.querySelectorAll("article").forEach(function (article) {
 /*3.Cuando el cursor esté sobre alguno de los siguientes elementos, seguir las instrucciones siguientes, y devolver dicho elemento a su estado original cuando salga el cursor.*/
 
 /*3.1 Imágenes: Cambia la imagen por el gif abracadabra.gif.*/
+
+const gifs = [
+  "assets/abracadabra.gif",
+  "assets/magic-2.gif",
+  "assets/magic-3.gif",
+  "assets/magic-4.gif",
+  "assets/magic-5.gif",
+  "assets/magic-6.gif"
+];
+const getRandomGifs = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+//Ejercicio 6
+
+/* Con click en una foto
 document
-  .getElementsByTagName("img")[2]
-  .addEventListener("mouseover", function () {
+.getElementsByTagName("img")[2]
+  .addEventListener("click", function () {
     document
       .getElementsByTagName("img")[2]
+      .setAttribute("src", getRandomGifs(gifs));
+  });*/
+  document.querySelectorAll("img").forEach(function (fotos) {
+    fotos.addEventListener("mouseover", function () {
+      fotos.setAttribute("src", getRandomGifs(gifs));
+      fotos.setAttribute("src", getRandomGifs(gifs));
+    });
+  });
+  document.querySelectorAll("img").forEach(function (fotos) {
+    fotos.addEventListener("mouseout", function () {
+      fotos.setAttribute("src", getRandomGifs(gifs));
+      fotos.setAttribute("src", getRandomGifs(gifs));
+    });
+  });
+/* Codigo del propio Ejercicio*/
+document
+  .getElementsByTagName("img")[3]
+  .addEventListener("mouseover", function () {
+    document
+      .getElementsByTagName("img")[3]
       .setAttribute("src", "assets/abracadabra.gif");
   });
 /*3.2 Párrafos: Cambia el color del texto y el de fondo por uno cualquiera.*/
@@ -60,6 +96,9 @@ document.querySelectorAll("p").forEach(function (parrafo) {
 });
 
 /*3.3 Bloques de article o section: Color de fondo distinto al de párrafo.*/
+
+//Ejercicio 5
+
 const pallete = [
   "#669bbc",
   "#003049",
@@ -76,7 +115,8 @@ const getRandomFondo = (array) => {
       getRandomFondo(pallete);
   
 
-/*document.getElementsByTagName("section")[0].addEventListener("mouseover", function () {
+/* Codigo del propio ejercicio
+document.getElementsByTagName("section")[0].addEventListener("mouseover", function () {
     document.getElementsByTagName("section")[0].style.color = "blue";
     document.getElementsByTagName("section")[0].style.backgroundColor =
       "yellow";
